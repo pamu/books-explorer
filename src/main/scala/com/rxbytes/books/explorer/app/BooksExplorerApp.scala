7f10f14dbb548721c71d69a9ef60eb40d64d5a5f
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-05-20 00:39:06 +0530

    books explorer application

---
+++ src/main/scala/com/rxbytes/books/explorer/app/BooksExplorerApp.scala
@@ -0,0 +1,15 @@
+package com.rxbytes.books.explorer.app
+
+import android.app.Application
+
+/**
+  * Created by pnagarjuna on 20/05/16.
+  */
+class BooksExplorerApp extends Application {
+
+  override def onCreate(): Unit = {
+    super.onCreate()
+    //Initialisation code goes here
+  }
+
+}
