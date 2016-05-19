595a03d0d95e71331f19c6c92c7c347b5b38876b
pamu <p_nagarjuna@students.iitmandi.ac.in>
2016-05-20 00:55:49 +0530

    hello-world textview

--- src/main/scala/com/rxbytes/books/explorer/ui/main/Layout.scala
+++ src/main/scala/com/rxbytes/books/explorer/ui/main/Layout.scala
@@ -1,6 +1,6 @@
 package com.rxbytes.books.explorer.ui.main
 
-import android.widget.LinearLayout
+import android.widget.{LinearLayout, TextView}
 import com.rxbytes.books.explorer.ui.commons.ToolbarLayout
 import macroid.ActivityContextWrapper
 import macroid.FullDsl._
@@ -9,7 +9,14 @@ import macroid.FullDsl._
   * Created by pnagarjuna on 05/05/16.
   */
 trait Layout extends ToolbarLayout {
+
   def layout(implicit context: ActivityContextWrapper) = {
-    getUi(l[LinearLayout](toolBarLayout()))
+    getUi(
+      l[LinearLayout](
+        toolBarLayout(),
+        w[TextView] <~ text("Hello world")
+      ) <~ vertical
+    )
   }
+
 }
